86fbe2dca6e5422b36602f4466e8b3ece28124e3
rahulkardile <rahulkardile321@gmail.com>
2024-11-21 19:52:58 +0530

    authentication complete

--- frontend/src/app/pages/login/login.component.ts
+++ frontend/src/app/pages/login/login.component.ts
@@ -1,13 +1,13 @@
 import { Component } from '@angular/core';
 import { AuthServiceService } from '../../core/auth-service.service';
 import { FormsModule } from '@angular/forms';
-import { Router } from '@angular/router';
+import { Router, RouterModule } from '@angular/router';
 import { UserLogin } from '../../interface/types';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [FormsModule],
+  imports: [FormsModule, RouterModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
@@ -20,17 +20,20 @@ export class LoginComponent {
 
   errorMessage = '';
 
-  constructor(private authService: AuthServiceService, private route: Router) { }
+  constructor(private authService: AuthServiceService, private router: Router) { }
 
   onSubmit() {
     this.authService.login(this.user).subscribe({
       next: (response: any) => {
-        localStorage.setItem("token", response.token),
-          this.route.navigate(["/hello"])
+        // localStorage.setItem("token", response.token)
+        console.log(response);
+        this.router.navigate(['/home']);
       },
+
       error: (error: any) => {
         this.errorMessage = error.message || "Internal server error";
       }
+
     })
   }
 
--- frontend/src/app/pages/register/register.component.ts
+++ frontend/src/app/pages/register/register.component.ts
@@ -1,12 +1,40 @@
 import { Component } from '@angular/core';
+import { FormsModule, NgModel } from '@angular/forms';
+import { AuthServiceService } from '../../core/auth-service.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-register',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './register.component.html',
   styleUrl: './register.component.css'
 })
 export class RegisterComponent {
-  
+  user = {
+    name: '',
+    email: '',
+    username: '',
+    phone: '',
+    location: '',
+    password: ''
+  };
+
+  errorMessage: String = "";
+
+  constructor(private authService: AuthServiceService, private router: Router){}
+
+  onSubmit(){
+    this.authService.register(this.user).subscribe({
+      next: (req: any)=>{
+        console.log(req);
+        this.router.navigate(["/login"]);
+      },
+      error: (err: any)=>{
+        this.errorMessage = err.message || 'Invalid credentials, please try again.';
+        console.log(err);
+      }
+    })
+  }
+
 }
